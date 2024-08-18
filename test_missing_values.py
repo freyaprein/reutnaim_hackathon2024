438a10f15ba27aa196b398d609032fe560a217b8
sofikrge <150954568+sofikrge@users.noreply.github.com>
2024-08-18 18:25:32 +0300

    first draft

---
+++ test_missing_values.py
@@ -0,0 +1,4 @@
+# does it equate to 50 mins
+# “manually”/separately check the mean and compare
+# test whether part_1 is equal to the original data —> nothing else got changed
+# compare with the missing data text file so it is approx the same
