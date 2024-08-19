cec67b0d67b9fc7da71f30e98dc5b8221ac8fb85
ShiriAr <shiriarnon@mail.tau.ac.il>
2024-08-19 18:33:17 +0300

    Update vis_indiv_files.py
    erased what I worked on with standard deviations... will add this code again but waiting for the corerct std dev files. pushing for now

--- vis_indiv_files.py
+++ vis_indiv_files.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def plot_participant_data(base_path):
 
     # Define the path to the 'c_individual recordings_test' folder within base_path
-    recordings_path = Path(base_folder) / "c_individual recordings_test"
+    recordings_path = Path(base_folder) / "clean_individual_recordings"
 
     # Get a list of available participant IDs (folder names within the 'c_individual recordings_test' folder)
     available_ids = [folder.name[4:] for folder in recordings_path.iterdir() if folder.is_dir()]
@@ -24,7 +24,7 @@ def plot_participant_data(base_path):
 
    
     # Define the folder path based on the participant ID
-    folder_name = f"c_individual recordings_test/c_rn{participant_id}"
+    folder_name = f"clean_individual_recordings/c_rn{participant_id}"
     folder_path = base_path / folder_name
 
  
@@ -35,6 +35,10 @@ def plot_participant_data(base_path):
     eda_file_path = folder_path / "c_EDA.csv"
     temp_file_path = folder_path / "c_TEMP.csv"
     tags_file_path = folder_path / "tags.csv"
+    sd_temp_file_path = folder_path / "sd_TEMP.csv"
+    sd_bvp_file_path = folder_path / "sd_BVP.csv"
+    sd_eda_file_path = folder_path / "sd_EDA.csv"
+    sd_hr_file_path = folder_path / "sd_HR.csv"
 
     # Check if the files exist
     if not bvp_file_path.is_file() or not hr_file_path.is_file() or not eda_file_path.is_file() or not temp_file_path.is_file() or not tags_file_path.is_file():
@@ -45,6 +49,7 @@ def plot_participant_data(base_path):
         hr_data = pd.read_csv(hr_file_path, skiprows=2, header=None)
         eda_data = pd.read_csv(eda_file_path, skiprows=2, header=None)
         temp_data = pd.read_csv(temp_file_path, skiprows=2, header=None)
+        sd_temp_data = pd.read_csv(sd_temp_file_path, header=None)
 
         # Load the value from starting sampling time in UTC from cell A1 of the BVP file - same value for all but HR file
         a1_value = pd.read_csv(bvp_file_path, nrows=1, header=None).iloc[0, 0]
@@ -64,6 +69,12 @@ def plot_participant_data(base_path):
         # Determine the common x-axis limits
         x_min = 0
         x_max = max(bvp_x_values[-1], hr_x_values[-1], eda_x_values[-1], temp_x_values[-1])
+
+
+        sdlow_temp = round(float(sd_temp_data.iloc[1, 2]), 1)
+        sdhigh_temp = round(float(sd_temp_data.iloc[1, 3]), 1)
+
+
 
         # Create subplots with a smaller figure size
         fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(9, 7), sharex=True)
@@ -91,6 +102,9 @@ def plot_participant_data(base_path):
         ax4.set_ylabel('TEMP Value')
         ax4.set_xlabel('Time (s)')
         ax4.grid(True)
+        ax4.axhline(y=sdlow_temp, color='black', linestyle='--', linewidth=1.5, label=f'C2: {sdlow_temp}')
+        ax4.axhline(y=sdhigh_temp, color='black', linestyle='--', linewidth=1.5, label=f'D2: {sdhigh_temp}')
+        ax4.legend(loc='upper right')
 
         # Plot adjusted vertical lines for tags on all subplots and add labels on the topmost plot
         for tag_x in adjusted_tag_x_values:
