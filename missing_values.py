17401e970e5faa04dfdad32fe0003a2d3c086d8e
sofikrge <150954568+sofikrge@users.noreply.github.com>
2024-08-19 13:04:41 +0300

    :)

--- missing_values.py
+++ missing_values.py
@@ -56,52 +56,52 @@ def missing_values(path):
                 print(f"Skipping {rn_folder_path}: unexpected content or structure.")
 
     
-    def determine_time_gap(recording1, recording2):
-        # Determine the time gap
-        time_gap = recording2['timestamp'].iloc[0] - recording1['timestamp'].iloc[-1]
-        return time_gap
-    
-        # NOTE: CHECK HOW WE NEED TO CALCULATE THE TIME!!!!!
-        # alternative 
-        # end_time_recording1 = recording1['time_column'].iloc[-1]
-        # start_time_recording2 = recording2['time_column'].iloc[0]
-        # time_gap = start_time_recording2 - end_time_recording1
-
-    # GENERATE MISSING VALUES AND TAG THEM IN A SEPARATE COLUMN
-    
-    def generate_dummy_rows(recording1, recording2, time_gap):
-        # Generate missing values
-        missing_values = pd.DataFrame(columns=recording1.columns)
-        for i in range(1, time_gap):
-            missing_values = missing_values.append(pd.Series(), ignore_index=True)
-        return missing_values
-    
-        # alternative
-        # missing_times = pd.date_range(end_time_recording1, start_time_recording2, periods=desired_number_of_missing_points, closed='right')
-        # missing_data = pd.DataFrame({'time_column': missing_times})
+        def determine_time_gap(recording1, recording2):
+            # Determine the time gap
+            time_gap = recording2['timestamp'].iloc[0] - recording1['timestamp'].iloc[-1]
+            return time_gap
         
-    def fill_in_missing_values(recording1, recording2, missing_values):
-        # Fill in missing values
-        filled_recording1 = pd.concat([recording1, missing_values, recording2], ignore_index=True)
-        return filled_recording1
+            # NOTE: CHECK HOW WE NEED TO CALCULATE THE TIME!!!!!
+            # alternative 
+            # end_time_recording1 = recording1['time_column'].iloc[-1]
+            # start_time_recording2 = recording2['time_column'].iloc[0]
+            # time_gap = start_time_recording2 - end_time_recording1
 
-        # alternative
-        # Add placeholder rows with NaN values
-        # for col in recording1.columns:
-        #     if col != 'time_column':
-        #         missing_data[col] = np.nan
-
-        # # Combine the first recording with the missing data
-        # combined_data = pd.concat([recording1, missing_data], ignore_index=True)
-
-        # # Interpolate the missing values
-        # combined_data.interpolate(method='linear', inplace=True)
+        # GENERATE MISSING VALUES AND TAG THEM IN A SEPARATE COLUMN
         
-        #Concatinate recordings
-        # final_combined_recording = pd.concat([combined_data, recording2], ignore_index=True)
-        # return final_combined_recording
+        def generate_dummy_rows(recording1, recording2, time_gap):
+            # Generate missing values
+            missing_values = pd.DataFrame(columns=recording1.columns)
+            for i in range(1, time_gap):
+                missing_values = missing_values.append(pd.Series(), ignore_index=True)
+            return missing_values
         
-    def save_in_new_csv_file (filled_recording1):
-        filled_recording1.to_csv('path_to_filled_recording.csv', index=False)
+            # alternative
+            # missing_times = pd.date_range(end_time_recording1, start_time_recording2, periods=desired_number_of_missing_points, closed='right')
+            # missing_data = pd.DataFrame({'time_column': missing_times})
+            
+        def fill_in_missing_values(recording1, recording2, missing_values):
+            # Fill in missing values
+            filled_recording1 = pd.concat([recording1, missing_values, recording2], ignore_index=True)
+            return filled_recording1
+
+            # alternative
+            # Add placeholder rows with NaN values
+            # for col in recording1.columns:
+            #     if col != 'time_column':
+            #         missing_data[col] = np.nan
+
+            # # Combine the first recording with the missing data
+            # combined_data = pd.concat([recording1, missing_data], ignore_index=True)
+
+            # # Interpolate the missing values
+            # combined_data.interpolate(method='linear', inplace=True)
+            
+            #Concatinate recordings
+            # final_combined_recording = pd.concat([combined_data, recording2], ignore_index=True)
+            # return final_combined_recording
+            
+        def save_in_new_csv_file (filled_recording1):
+            filled_recording1.to_csv('path_to_filled_recording.csv', index=False)
 
 
