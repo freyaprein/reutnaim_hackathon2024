c2140dca005e08b7804e1fbd363355add3e74fbf
ShiriAr <shiriarnon@mail.tau.ac.il>
2024-08-20 20:30:17 +0300

    working main file!!!

--- src/empatica_processing/reut_naim_hackathon_project_2.py
+++ src/empatica_processing/reut_naim_hackathon_project_2.py
@@ -8,9 +8,14 @@ def main():
     base_folder = "/Users/shiriarnon/Documents/TAU/Courses/Year_1_(23-24)/Semester_2/Python_for_neuroscience/Hackathon/Hackathon_files_adapt_lab"
     processor = UnusualSubjectDataProcessor(base_folder) # Create an instance of UnusualSubjectDataProcessor
     processor.process_subjects() # Process the subjects
+    filter = OutliersDataProcessor(base_folder)
+    filter.process_individual_recordings()
+    plotter = ParticipantDataPlotter(base_folder)
+    plotter.plot_participant_data()
+
 
 
 
 if __name__ == "__main__":
     # Code here will only run if outliers.py is executed directly
-    some_function()
+    main()
